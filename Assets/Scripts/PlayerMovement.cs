c9fa6c303e7ab181c33f1caa30a8c8f34b84dc2c
Aadhya Puttur <putturaadhya@gmail.com>
2026-03-18 14:19:16 -0700

    player moves left and right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,23 @@
-using Systems.Collections;
-using Systems.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
 
+    void Update()
+    {
+        float input = 0f;
 
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+                input = -1f;
+            else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+                input = 1f;
+        }
+
+        Vector3 movement = new Vector3(input * speed * Time.deltaTime, 0f, 0f);
+        transform.Translate(movement);
+    }
+}
