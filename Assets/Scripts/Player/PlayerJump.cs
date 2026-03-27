12e940a9a04f82f9b95440bccadc49d5d931ecc5
Aadhya Puttur <putturaadhya@gmail.com>
2026-03-27 14:55:17 -0700

    Jump mechanic for player

---
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+        [SerializeField] private Rigidbody2D rigidbody2D;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private float jumpForce = 6;
+        private bool isGrounded;
+        private float playerHalfHeight;
+
+
+        private void Start()
+        {
+                playerHalfHeight = spriteRenderer.bounds.extents.y;
+        }
+        
+        void Update()
+        {
+               
+                if (Input.GetButtonDown("Jump") && GetIsGrounded())
+                {
+                        Jump();
+                }
+                
+        }
+
+        private bool GetIsGrounded()
+        {
+                return Physics2D.Raycast(transform.position, Vector2.down, playerHalfHeight + 0.001f, LayerMask.GetMask("Ground"));
+        }
+
+        private void Jump()
+        {
+                rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+}
